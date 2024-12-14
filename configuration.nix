49f8a749c3bed8f0d9adb5a1586dbbf1a0d55e63
maresyp <mattq.2000@icloud.com>
2024-12-14 18:03:57 +0100

    change syntax error

--- configuration.nix
+++ configuration.nix
@@ -74,7 +74,7 @@
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
-      extraGroups = ["wheel", "media"];
+      extraGroups = ["wheel" "media"];
     };
   };
 
