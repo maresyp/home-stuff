0a1acdb1bd3c0ee959760ad7d6dfcff784f32f65
maresyp <mattq.2000@icloud.com>
2024-12-14 18:06:20 +0100

    test2

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,3 @@
-# This is your system's configuration file.
-# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 {
   inputs,
   lib,
@@ -92,5 +90,5 @@
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "23.05";
+  system.stateVersion = "24.05";
 }
